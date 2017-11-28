8ad9d0c13c73705047c1a66fb12c7025497e7c41
joter822 <elo_junior@wp.pl>
2017-11-28 17:31:04 +0100

    test selenium

---
+++ automatyzacja_day1/ClassLibrary1/Example.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+using System.Linq;
+
+namespace SeleniumTests
+{
+    public class Example : IDisposable
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        public Example()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+         //   driver.Manage().Timeouts()
+            baseURL = "https://www.google.pl/";
+            verificationErrors = new StringBuilder();
+        }
+
+
+
+        [Fact]
+        public void TheExampleTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/?gfe_rd=cr&dcr=0&ei=4FsdWtrJL8mv8wei1rjgAw");
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("codesprinters");//znajduje pierwszy element na stronie
+            driver.FindElement(By.Id("lst-ib")).Submit();
+            driver.FindElement(By.LinkText("Code Sprinters -")).Click();
+            //// ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=@color | ]]
+
+            var element = driver.FindElement(By.LinkText("Poznaj nasze podejście"));
+            Assert.NotNull(element);
+            driver.FindElement(By.LinkText("Akceptuję")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2)); //czeka 5 sekund aż nie zniknie
+            wait.Until(ExpectedConditions.InvisibilityOfElementWithText(By.LinkText("Akceptuję"), "Akceptuję"));
+
+            waitForElementPresent(By.LinkText("Poznaj nasze podejście"), 0);
+
+            var elements = driver.FindElements(By.LinkText("Poznaj nasze podejście"));
+            Assert.Single(elements);
+
+            driver.FindElement(By.LinkText("Poznaj nasze podejście")).Click();
+
+            System.Threading.Thread.Sleep(2);//czeka zawsze 2 sekundy
+
+
+
+            //ver 1
+            Assert.Contains("WIEDZA NA PIERWSZYM MIEJSCU", driver.PageSource);//wyszukiwanie tekstu na stronie
+
+            //ver2
+
+            Assert.Single(driver.FindElements(By.TagName("h2"))
+                .Where(tag => tag.Text == "WIEDZA NA PIERWSZYM MIEJSCU"));
+            //wyszukuje tagi o nazwie h2, w zwróconej kolekcji tagów wyszukuje tekstu XXX
+            //single -> sprawdza czy w zwróconej kolekcji jest dokładnie 1 element
+            
+        }
+            protected void waitForElementPresent(IWebElement by, int seconds)
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+                wait.Until(ExpectedConditions.ElementToBeClickable(by));
+            }
+            protected void waitForElementPresent(By by, int seconds)
+            {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+            }
+        //Assert.Equal("Code Sprinters -", driver.Title);
+
+        //private bool IsElementPresent(By by)
+        //{
+        //    try
+        //    {
+        //        driver.FindElement(by);
+        //        return true;
+        //    }
+        //    catch (NoSuchElementException)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //private bool IsAlertPresent()
+        //{
+        //    try
+        //    {
+        //        driver.SwitchTo().Alert();
+        //        return true;
+        //    }
+        //    catch (NoAlertPresentException)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //private string CloseAlertAndGetItsText()
+        //{
+        //    try
+        //    {
+        //        IAlert alert = driver.SwitchTo().Alert();
+        //        string alertText = alert.Text;
+        //        if (acceptNextAlert)
+        //        {
+        //            alert.Accept();
+        //        }
+        //        else
+        //        {
+        //            alert.Dismiss();
+        //        }
+        //        return alertText;
+        //    }
+        //    finally
+        //    {
+        //        acceptNextAlert = true;
+        //    }
+        //}
+
+        public void Dispose()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.Equal("", verificationErrors.ToString());
+        }
+        
+    }
+}

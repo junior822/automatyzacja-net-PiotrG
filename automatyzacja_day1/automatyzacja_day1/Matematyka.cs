6ecb3235d5348fe79ad8b9852025f8bb4511bdbf
joter822 <elo_junior@wp.pl>
2017-11-28 11:25:47 +0100

    drugi commit

---
+++ automatyzacja_day1/automatyzacja_day1/Matematyka.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automatyzacja_day1
+{
+    public class Matematyka
+    {
+        public double Add(double x, double y)
+        {
+            return (x + y);            
+        }
+        public double Substract(double x, double y)
+        {
+            return (x - y);
+        }
+        public double Multiply(double x, double y)
+        {
+            return (x * y);
+        }
+        public double Devide(double x, double y)
+        
+        {
+            return (x / y);
+        }
+    }
+}
